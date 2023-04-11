55c15257455a13110556a4564b44d70a36233c1f
Hailong Han <hello.HAN@Honeywell.com>
2023-04-11 13:40:01 +0800

    feat:更新suite折线图

--- controllers/common.js
+++ controllers/common.js
@@ -2,46 +2,22 @@ const Result = require("../utils/tool");
 const express = require("express");
 const router = express.Router();
 const request = require("request");
-let result = require("./demo.json");
+// let devices = require("./devices.json");
+// let customers = require("./customers.json");
 router.all("/hone/statisticsReport", async (req, res) => {
   let params = req.body;
   if (Object.keys(params).length == 0) {
     params = req.query;
   }
-  let list = [];
-  let data = result.data
-  if (data&&data.length) {
-    if (!params.lastDate && !params.startDate) {
-      list = data;
-    } else {
-      for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        if (params.lastDate) {
-          if (params.startDate) {
-            if (element.name>=params.lastDate&&element.name>=params.startDate&&element.name<=params.endDate) {
-              list.push(element)  
-            }
-          }else{
-            if (element.name>=params.lastDate){
-              list.push(element)
-            }
-          }
-        }else{
-          if (element.name>=params.startDate&&element.name<=params.endDate) {
-            list.push(element)  
-          }
-        }
-      }
-    }
+  if (!params.type||!params.env) {
+    res.json(
+      new Result({
+        code: 500,
+        message: "参数缺失",
+      })
+    );
+    return
   }
-  res.json(
-    new Result({
-      data: list,
-      message: "success",
-    })
-  );
-
-  return;
   let url = `https://ssdm-manage-qa.ss.honeywell.com.cn/statisticsReport?type=${params.type}&env=${params.env}&timezone=UTC-5&format=yyyy-MM-dd`;
   request(
     {
@@ -56,7 +32,30 @@ router.all("/hone/statisticsReport", async (req, res) => {
       if (!error && response.statusCode == 200) {
         let result = JSON.parse(body);
         let list = [];
-        if (result.data.length) {
+        let data = result.data
+        if (data&&data.length) {
+          if (!params.lastDate && !params.startDate) {
+            list = data;
+          } else {
+            for (let index = 0; index < data.length; index++) {
+              const element = data[index];
+              if (params.lastDate) {
+                if (params.startDate) {
+                  if (element.name>=params.lastDate&&element.name>=params.startDate&&element.name<=params.endDate) {
+                    list.push(element)  
+                  }
+                }else{
+                  if (element.name>=params.lastDate){
+                    list.push(element)
+                  }
+                }
+              }else{
+                if (element.name>=params.startDate&&element.name<=params.endDate) {
+                  list.push(element)  
+                }
+              }
+            }
+          }
         }
         res.json(
           new Result({
@@ -74,13 +73,6 @@ router.all("/hone/statisticsReport", async (req, res) => {
       }
     }
   );
-
-  res.json({
-    data: [
-      { name: "Cloud", value: 146 },
-      { name: "On-premise", value: 28 },
-    ],
-  });
 });
 
 module.exports = router;
